a1f4867569ad01150ed1efcf178bd22ada0ac3ec
johnairafeleo <32088082+johnairafeleo@users.noreply.github.com>
2025-04-16 08:57:16 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Container } from '@/components/common/container'
 import React from 'react'
 
 export default function Page() {
   return (
     <div className='py-24'>
-      <div className='container max-w-3xl'>
-        <h1 className='text-3xl font-bold'>Portfolio</h1>
-      </div>
+      <Container>
+        <h1 className='px-5 text-3xl font-bold'>Portfolio</h1>
+      </Container>
     </div>
   )
 }
