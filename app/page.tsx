06ca0808e6587692adf0e3783932b92dfcb8b71f
johnairafeleo <32088082+johnairafeleo@users.noreply.github.com>
2025-04-22 09:16:51 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { Container } from '@/components/common/container'
 import Intro from '@/components/intro'
-import React from 'react'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
-export default function Page() {
+export default function Home() {
+  const content = `
+# this is a markdown heading
+`
   return (
-    <div className='py-24'>
+    <section className='py-24'>
       <Container>
         <Intro />
+        <MDXRemote source={content} />
       </Container>
-    </div>
+    </section>
   )
 }
